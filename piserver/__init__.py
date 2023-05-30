547bdf775a9bec6ef848f96bbb5170751468629f
Brenden Pelkie <bgpelkie@gmail.com>
2023-05-30 15:52:41 -0700

    move to picam 2

--- piserver/__init__.py
+++ piserver/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, send_from_directory
 import os
-from picamera import PiCamera
+from picamera2 import Picamera2
 import time
 
 scratch_fp = '~/'
@@ -9,7 +9,7 @@ def create_app():
 
     app = Flask(__name__)
 
-    camera = PiCamera()
+    camera = Picamera2()
 
     @app.route('/image')
     def image():
@@ -17,7 +17,7 @@ def create_app():
         Take an image and return it over the function
         """
         time.sleep(2)
-        camera.capture(scratch_fp + 'capture.jpg')
+        camera.start_and_capture_file(scratch_fp + 'capture.jpg')
 
         return send_from_directory(scratch_fp + 'capture.jpg')
     
