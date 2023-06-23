9c538ba31e8fd8208a609b1518b1ad5c871ca1bc
Brenden Pelkie <bgpelkie@gmail.com>
2023-06-23 15:13:46 -0700

    flesh out bayeian optimization loop

--- jubileedemo/user_functions.py
+++ jubileedemo/user_functions.py
@@ -1,40 +1,111 @@
 # things that users/students are going to be calling. Should take arguments that allow experimentation, manage BO/sampling related stuff
+import numpy as np
+import image_processing as img
+from bayesopt import bayesian_optimzer, acquisitions
 
-def sample_point(RGB: tuple, volume: float):
+def sample_point(jubilee, RYB: tuple, volume: float, well):
     """
     Sample a specified point. 
 
     Inputs:
-    RGB (tuple) - (R, G, B) values - either 0-1 or 0-255.
+    RYB (tuple) - (R, Y, B) values - either 0-1 or 0-255.
     volume: total sample volume
+
+    Returns:
+    -------
+    RGB - tuple RGB value of resulting solution
     """
+    RYB = list(RYB)
+    # get the volumes of each color
+    if np.isclose(sum(RYB), 1):
+        pass
+    elif np.isclose(sum(RYB) ,255):
+        RYB = [i/255 for i in RYB]
+    else:
+        raise AssertionError('Error: Volume fractions of RYB must add to 1 or 255')
+    
+    volumes = [vf*volume for vf in RYB]    
+    
+
+    # convert color to pippette index
+    # hard-coded pippette indices for now
+    red = 0
+    yellow = 1
+    blue = 2
+
+    # get the well that we want to mix in
+    # assume this is passed to function
+
+    # dispense volumes into well
+    jubilee.dispense(well, volumes[0], red)
+    jubilee.dispense(well, volumes[1], yellow)
+    jubilee.dispense(well, volumes[2], blue)
+    # measure well with camera
+    image = jubilee.well_image(well)
+
+    # do post-processing 
+    RGB = img.process_image(image)
+    
+    return RGB
 
 
-def BO_campaign(initial_data, acquisistion_function, number_of_iterations, jubilee_instance):
+def BO_campaign(initial_data, acquisition_function, number_of_iterations, jubilee):
     """
     This should be a child-safed way to run BO on the platform
     """
+    sample_volume = 10 # mL
+    n_points = 101
 
     # define possible sampling grid
+    available_points = get_constrained_points(n_points)
     # we know we are working with a 3-variable constrained design space here so can just hard-code that
-
     # instantiate a bayesian optimizer object
+    bo = bayesian_optimzer.BayesianOptimizer(None, acquisition_function, None, initial_data)
 
     # check that we have enough sample wells for the number of iterations we want to run 
 
-    # for each iteration:
+    # get first set of points from model
+    query_point = bo.campaign_iteration(None, None)
+
+    for i in range(number_of_iterations):
         # query point from BO model
-
+        # get well
+        print(f'Starting iteration {i}')
+        well = jubilee.next_sample_well()
         # run point in real world
+        print(f'Dispensing into well {well}')
+        print('RYB values tested: {query_point}')
+        new_color = sample_point(jubilee, query_point, sample_volume, well)
 
-        # call the deck manager to get the next well location
+        print('RGB values observed: {RGB}')
+        query_point = bo.campaign_iteration(query_point, new_color)
 
-        # call the prep_sample function to build the sample
 
-        # call the camera motion control function to go take a picture of the sample
 
-        # call the image processing module to get the RGB values
 
-        # check convergence criteria
+def get_constrained_points(n_points):
+    """
+    Get the available points in 3-dimensional sample space with volumetric mixing constraint
+    """
 
-        # update BO model, data
+
+    R = np.linspace(0, 1, n_points)
+    Y = np.linspace(0, 1, n_points)
+    B = np.linspace(0,1, n_points)
+
+    # do a brute force constrained sampling to get points in the design space
+    test_arr = np.array(np.meshgrid(R, Y, B)).T.reshape(-1,3)
+    indices = np.where(test_arr.sum(axis = 1) != 1)[0]
+    testable_points = np.delete(test_arr, indices, axis = 0)
+
+    return testable_points
+
+def initial_random_sample(testable_points, n_sample = 12):
+    """
+    Sample n_sample points from testable_points to get initial data
+    """
+    rng = np.random.default_rng(seed = 4)
+    selected_inds = rng.integers(0, len(testable_points), n_sample)
+    selected_points = testable_points[selected_inds, :]
+   
+    return selected_points
